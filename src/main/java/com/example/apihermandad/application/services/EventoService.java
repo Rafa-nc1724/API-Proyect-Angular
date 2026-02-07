1c00ac55450e1365321b684f0b67d32e334f7216
Rafa-nc1724 <rniecam350@iesmarquesdecomares.com>
2026-02-07 11:33:45 +0100

    refactor: create news metod of EventController

--- src/main/java/com/example/apihermandad/application/services/EventoService.java
+++ src/main/java/com/example/apihermandad/application/services/EventoService.java
@@ -2,8 +2,12 @@ package com.example.apihermandad.application.services;
 
 import com.example.apihermandad.application.dto.EventoDto;
 import com.example.apihermandad.application.mapper.EventoMapper;
+import com.example.apihermandad.domain.entity.Evento;
+import com.example.apihermandad.domain.entity.Grupo;
 import com.example.apihermandad.domain.repository.EventoRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -19,10 +23,56 @@ public class EventoService {
         this.eventoMapper = eventoMapper;
     }
 
+    //Este servicio es el único público
     public List<EventoDto> getAllEventos() {
         return eventoRepository.findAll()
                 .stream()
                 .map(eventoMapper::toDto)
                 .toList();
+    }
+
+    //Servicios privados solo para admin, junta y capataz.
+    public EventoDto create(EventoDto dto){
+        Evento event= new Evento();
+        event.setTitulo(dto.titulo());
+        event.setDescripcion(dto.descripcion());
+        event.setFecha(dto.fecha());
+        if (dto.grupoId() != null){
+            Grupo grup = new Grupo();
+            grup.setId(dto.grupoId());
+            grup.setId(dto.grupoId());
+            event.setIdGrupo(grup);
+        }
+        return eventoMapper.toDto(eventoRepository.save(event));
+    }
+
+    public EventoDto update(Integer id, EventoDto dto) {
+        Evento evento = eventoRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "Evento no encontrado"
+                ));
+
+        evento.setTitulo(dto.titulo());
+        evento.setDescripcion(dto.descripcion());
+        evento.setFecha(dto.fecha());
+
+        if (dto.grupoId() != null) {
+            Grupo grupo = new Grupo();
+            grupo.setId(dto.grupoId());
+            evento.setIdGrupo(grupo);
+        }
+
+        return eventoMapper.toDto(eventoRepository.save(evento));
+    }
+
+    public void delete(Integer id) {
+        if (!eventoRepository.existsById(id)) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    "Evento no encontrado"
+            );
+        }
+        eventoRepository.deleteById(id);
     }
 }
--- src/main/java/com/example/apihermandad/infrastructure/controller/EventoController.java
+++ src/main/java/com/example/apihermandad/infrastructure/controller/EventoController.java
@@ -2,6 +2,8 @@ package com.example.apihermandad.infrastructure.controller;
 
 import com.example.apihermandad.application.dto.EventoDto;
 import com.example.apihermandad.application.services.EventoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,11 +19,63 @@ public class EventoController {
     }
 
     /**
-     * GET /api/eventos
+     * GET /api/events
      * Endpoint público
      */
     @GetMapping
     public List<EventoDto> getAllEventos() {
         return eventoService.getAllEventos();
     }
+
+    /**
+     * POST /api/events
+     * Ejemplo: http://localhost:8080/api/events
+     * Body:
+     * {
+     *   "titulo": "Ensayo general de la virgen",
+     *   "descripcion": "Se realizará en la calle Trasmonjas",
+     *   "fecha": "2026-02-12",
+     *   "grupoId": 8
+     * }
+     * Headers:
+     * Authorization-->Bearer "token del admin, junta o capataz"
+     * EndPoint privado
+     */
+    @PostMapping
+    @PreAuthorize("hasAnyRole('ADMIN','JUNTA','CAPATAZ')")
+    public ResponseEntity<EventoDto> create(@RequestBody EventoDto dto) {
+        return ResponseEntity.ok(eventoService.create(dto));
+    }
+
+    /**
+     * PUT /api/events
+     * Ejemplo:http://localhost:8080/api/events/7
+     * Body:{
+     *   "titulo": "Ensayo general de la virgen",
+     *   "descripcion": "Se realizará en la palaza de San Pedro",
+     *   "fecha": "2026-02-12",
+     *   "grupoId": 8
+     * }
+     * Headers:
+     * Authorization-->Bearer "token del admin, junta o capataz"
+     * Endpoint privadao
+     */
+    @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN','JUNTA','CAPATAZ')")
+    public ResponseEntity<EventoDto> update(@PathVariable Integer id, @RequestBody EventoDto dto) {
+        return ResponseEntity.ok(eventoService.update(id,dto));
+    }
+
+    /**
+     * DELETE /api/events
+     * Ejemplo:http://localhost:8080/api/events/7
+     * Endpoint provado
+     */
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN','JUNTA','CAPATAZ')")
+    public ResponseEntity<EventoDto> delete(@PathVariable Integer id) {
+        eventoService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
