4210519ef63fda99460092616611d64eb4720cc8
siwdie <codyjmc@gmail.com>
2026-02-19 20:20:30 +0100

    fix: change noticia mapper to admit image value as null

--- src/main/java/com/example/apihermandad/application/mapper/NoticiaMapper.java
+++ src/main/java/com/example/apihermandad/application/mapper/NoticiaMapper.java
@@ -7,7 +7,7 @@ import org.mapstruct.*;
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface NoticiaMapper {
 
-    @Mapping(target = "imageUrl", expression = "java(\"/api/image/\" + noticia.getImage().getId())")
+    @Mapping(target = "imageUrl", expression = "java(noticia.getImage() != null ? \"/api/image/\" + noticia.getImage().getId() : null)")
     NoticiaDto toDto(Noticia noticia);
 
 }
